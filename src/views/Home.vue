23a7b10118c0be296173ec32d0abf3207f5968cc
Cameron Romo <cromo1225@gmail.com>
2021-06-01 14:22:17 -0600

    Add a landing page with styles and inputs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,116 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="feature-card">
+      <router-link to="/movie/tt0409591">
+        <img src="https://cdn11.bigcommerce.com/s-b72t4x/images/stencil/1280x1280/products/11169/20874/24_1075_Naruto_Group__97803.1415650378.jpg?c=2" alt="Naruto Poster" class="featured-img">
+        <div class="detail">
+          <h3>Naruto</h3>
+          <p>Naruto Uzumaki, a mischievous adolescent ninja, struggles as he searches for 
+          recognition and dreams of becoming the Hokage, the village's leader and strongest ninja.
+          </p>
+        </div>
+      </router-link>
+    </div>
+    <form @submit.prevent="" class="search-box">
+      <input type="text" placeholder="What are you looking for?">
+      <input type="submit" value="Search">
+    </form>
+
+    <div class="movies-list">MOVIES</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+
+<style lang="scss">
+.home {
+  .feature-card {
+    position: relative;
+
+    .featured-img {
+      display: block;
+      width: 100%;
+      height: 300px;
+      object-fit: cover;
+
+      position: relative;
+      z-index: 0;
+    }
+
+    .detail {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      z-index: 1;
+
+      h3 {
+        color: #fff;
+        margin-bottom: 16px;
+      }
+
+      p {
+        color: #fff;
+      }
+    }
+  }
+
+  .search-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 16px;
+
+    input {
+      display: block;
+      appearance: none;
+      border: none;
+      outline: none;
+      background: none;
+
+      &[type='text'] {
+        width: 100%;
+        color: #fff;
+        background-color: #496583;
+        font-size: 20px;
+        padding: 10px 16px;
+        border-radius: 8px;
+        margin-bottom: 15px;
+        transition: 0.4s;
+
+        &::placeholder {
+          color: #f3f3f3;
+        }
+
+        &:focus {
+          box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
+        }
+      }
+
+      &[type='submit'] {
+        width: 100%;
+        min-width: 300px;
+        background-color: #42B883;
+        padding: 16px;
+        border-radius: 8px;
+        color: #fff;
+        font-size: 20px;
+        text-transform: uppercase;
+        transition: 0.4;
+
+        &:active {
+          background-color: #3B8070;
+        }
+      }
+    }
+  }
+}
+</style>
