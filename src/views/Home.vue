310de90b070381b08b10a1bd425ec4968f4d1279
Cameron Romo <cromo1225@gmail.com>
2021-06-01 14:52:58 -0600

    Add fetch method to call server for movies

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,8 +11,8 @@
         </div>
       </router-link>
     </div>
-    <form @submit.prevent="" class="search-box">
-      <input type="text" placeholder="What are you looking for?">
+    <form @submit.prevent="searchMovies()" class="search-box">
+      <input type="text" placeholder="What are you looking for?" v-model="search">
       <input type="submit" value="Search">
     </form>
 
@@ -21,9 +21,31 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+import env from '@/env';
 
 export default {
+  setup() {
+    const search = ref("");
+    const movies = ref([])
 
+    const searchMovies = () => {
+      if (search.value != "") {
+        fetch(`http://www.omdbapi.com/?apikey=${env.apiKey}&s=${search.value}`)
+          .then(response => response.json())
+          .then(data => {
+            movies.value = data.Search;
+            search.value = "";
+          });
+      }
+    }
+
+    return {
+      search,
+      movies,
+      searchMovies
+    }
+  }
 }
 </script>
 
